d351a43fc157db6a72739fdd0e8be60a34b8a8c6
Andrei Saldorfean <andreisaldorfean@gmail.com>
2024-11-30 02:37:39 +0200

    display the buffer load time

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -3,3 +3,13 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
     vim.cmd("Neotree close")
   end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local start_time = vim.fn.reltime()
+        vim.schedule(function()
+            local elapsed_time = vim.fn.reltimefloat(vim.fn.reltime(start_time)) * 1000
+            print(string.format("Buffer loaded in %.0f ms", elapsed_time))
+        end)
+    end,
+})
