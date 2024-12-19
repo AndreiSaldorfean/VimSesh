f611d7b7b90cc0d8a5b574e790f9fc5166cacb3c
Andrei Saldorfean <andreisaldorfean@gmail.com>
2024-12-19 23:07:10 +0200

    add new plugins
    	- add noice
    	- replace cmp completion with blink
    	- tweak ibl
    	- add new layout for dap and virtual text
    	- show whitespaces
    	- integrate tmux
    	- add doxygen syntax highlight
    	- make neovim transparent
    	- add commit graph
    	- add vim-fugitive(only used for one feature)
    	- add diffview

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -19,20 +19,197 @@ vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
   {
-    "folke/neodev.nvim",
-    opts = {},
-    config = function()
-      require("neodev").setup({
-        library = { plugins = { "nvim-dap-ui" }, types = true },
+    'sindrets/diffview.nvim',
+    config = function ()
+      require("diffview").setup({
+        enhanced_diff_hl = true,
       })
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gread", "Gwrite", "Gdiffsplit", "Gvdiffsplit", "Gclog" }, -- Lazy-load on commands
+    keys = {
+      { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },                       -- Shortcut for Git status
+    },
+    config = function()
+      -- Optional: Additional settings or keymaps for Fugitive
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame" })
+    end,
+  },
+  {
+    "rbong/vim-flog",
+    lazy = true,
+    cmd = { "Flog", "Flogsplit", "Floggit" },
+    dependencies = {
+      "tpope/vim-fugitive", },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      -- Optional, you don't have to run setup.
+      require("transparent").setup({
+        -- table: default groups
+        groups = {
+          'Normal',
+          'NormalNC',
+          'Comment',
+          'Constant',
+          'Special',
+          'Identifier',
+          'Statement',
+          'PreProc',
+          'Type',
+          'Underlined',
+          'Todo',
+          'String',
+          'Function',
+          'Conditional',
+          'Repeat',
+          'Operator',
+          'Structure',
+          'LineNr',
+          'NonText',
+          'SignColumn',
+          -- 'CursorLine',
+          -- 'CursorLineNr',
+          'StatusLine',
+          'EndOfBuffer',
+          'WinSeparator'
+        },
+        extra_groups = {},
+        exclude_groups = {},
+        on_clear = function() end,
+      })
+      require("transparent").clear()
+    end
+  },
+  {
+    "paopaol/cmp-doxygen",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-treesitter/nvim-treesitter-textobjects"
+    },
+    config = function()
+      require "cmp".setup({ sources = { name = "doxygen" } })
+    end
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup({})
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    }
+  },
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({
+        fzf_opts = {
+          ['--tiebreak'] = 'end'
+        }
+      })
+    end
+  },
+  {
+    'johnfrankmorgan/whitespace.nvim',
+    config = function()
+      require('whitespace-nvim').setup({
+        highlight = 'DiffDelete',
+        ignored_filetypes = { 'floggraph', 'fzf', 'neo-tree', 'toggleterm', 'TelescopePrompt', 'Trouble', 'help', 'dashboard' },
+        ignore_terminal = true,
+        return_cursor = true,
+      })
+
+      -- remove trailing whitespace with a keybinding
+      vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
     end
   },
   -- Debugger
   {
     "mfussenegger/nvim-dap",
-    dependencies = { "williamboman/mason.nvim", "jay-babu/mason-nvim-dap.nvim" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jay-babu/mason-nvim-dap.nvim",
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+    },
     config = function()
       require("dapui").setup {}
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup({
+        render = {
+          max_type_length = 0,
+          max_value_lines = 3,
+        },
+        layouts = {
+          {
+            elements = {
+              {
+                id = "scopes",
+                size = 0.5
+              },
+              {
+                id = "watches",
+                size = 0.25
+              } },
+            position = "left",
+            size = 40
+          },
+          {
+            elements = { {
+              id = "repl",
+              size = 0.5
+            }, {
+              id = "console",
+              size = 0.5
+            } },
+            position = "bottom",
+            size = 10
+          }
+        },
+      })
+      require("dap-go").setup()
+
+      require("nvim-dap-virtual-text").setup {
+        -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+          end
+
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+
+          return " " .. variable.value
+        end,
+      }
       require('mason-nvim-dap').setup({
         ensure_installed = { 'cpptools' },
       })
@@ -62,9 +239,10 @@ require("lazy").setup({
           char = '│',
           smart_indent_cap = false,
           repeat_linebreak = false,
-        }
+        },
+        scope = { enabled = false }
       }
-    end
+    end,
   },
   -- Remember last session
   {
@@ -101,16 +279,61 @@ require("lazy").setup({
   {
     "lewis6991/gitsigns.nvim"
   },
+  {
+    'saghen/blink.cmp',
+    lazy = false, -- lazy loading handled internally
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    -- use a release tag to download pre-built binaries
+    version = 'v0.*',
+    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        -- optionally disable cmdline completions
+        -- cmdline = {},
+      },
+
+      -- experimental signature help support
+      -- signature = { enabled = true }
+    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" }
+  },
   -- LSP
   {
     "onsails/lspkind.nvim",
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path'
   },
   {
     "L3MON4D3/LuaSnip",
@@ -154,9 +377,13 @@ require("lazy").setup({
       })
     end
   },
-  -- STATUS BAR
   {
-    "famiu/feline.nvim"
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+      })
+    end,
+    event = "VeryLazy",
   },
   -- COLORSCHEME
   {
@@ -174,15 +401,9 @@ require("lazy").setup({
       'lewis6991/gitsigns.nvim',
       'nvim-tree/nvim-web-devicons',
     },
-    init = function() vim.g.barbar_auto_setup = true end,
+    init = function()
+      vim.g.barbar_auto_setup = true
+    end,
     version = '^1.0.0'
-  },
-  -- TERMINAL
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-      require("toggleterm").setup {}
-    end
-  },
+  }
 })
